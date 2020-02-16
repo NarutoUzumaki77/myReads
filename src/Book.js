a41350a2c8d7c08642f6f71b6d215c4ed87c3cfa
Gilbert Nwankwo <kellyemeka7@gmail.com>
2020-02-16 06:01:56 -0500

    updated ReadMe with application introduction

--- src/Book.js
+++ src/Book.js
@@ -7,7 +7,11 @@ class Book extends React.Component {
         return (
             <div className="book">
             <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+              <div className="book-cover" style={{ 
+                width: 128, 
+                height: 193, 
+                backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
+              </div>
               <div className="book-shelf-changer">
                 <select 
                     defaultValue={this.props.shelf}
